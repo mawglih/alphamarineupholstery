a259794e3330281a903932c7d466786dbe218858
Oleg Markoff <mawglih@gmail.com>
2020-03-16 10:01:37 -0400

    mobile styles

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,15 +4,17 @@ import styles from './home.css';
 
 const index = () => (
   <div className={styles.container}>
+    <h1 className={styles.header}>Alphamarineupholstery - your boat renovation shop</h1>
+    <div className={styles.hero}></div>
     <div className={styles.row}>
       <div className={styles.image}>
-        <img src="" alt="awning"/>
+        <img src="https://amu-images.s3.us-east-2.amazonaws.com/front/enc_main.png" alt="awning"/>
       </div>
       <div className={styles.image}>
-        <img src="" alt="enclosure"/>
+        <img src="https://amu-images.s3.us-east-2.amazonaws.com/front/otf_main.png" alt="enclosure"/>
       </div>
       <div className={styles.image}>
-        <img src="" alt="upholstery"/>
+        <img src="https://amu-images.s3.us-east-2.amazonaws.com/front/uph_main.png" alt="upholstery"/>
       </div>
     </div>
     <div className={styles.row}>
