bafa996f7342ec378236536ccd46e7547a8af8a8
Oleg Markoff <mawglih@gmail.com>
2020-03-26 20:01:28 -0400

    update with modals

--- src/components/AllFotos/index.js
+++ src/components/AllFotos/index.js
@@ -3,12 +3,13 @@ import aws from '../../constants';
 
 const AllFotos = ({
   classname1,
-  classname2
+  classname2,
+  onImageClick,
 }) => {
   const imagePath = aws.root;
   const Images = aws.fotos.map((image, ind) => {
     return (
-      <div key={ind} className={classname2}>
+      <div key={ind} className={classname2} onClick={() => onImageClick(image)}>
         <img src={`${imagePath}${image}.png`} alt={image} />
       </div>
     )
